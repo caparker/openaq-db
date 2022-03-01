fe788e0cc592d12f2131d49324f2b9fa3e318553
Christian Parker <cparker@talloaks-environmental.com>
2022-03-01 15:50:15 -0800

    Updating the deployment method

--- cdk/app.py
+++ cdk/app.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python3
-
-import docker
-from aws_cdk import (
-    core,
-    aws_ec2 as _ec2,
-)
-from aws_cdk.aws_lambda_event_sources import S3EventSource
-
-import pathlib
 from pathlib import Path
+
+import aws_cdk
+from aws_cdk import (
+    aws_ec2 as _ec2,
+    Stack,
+    CfnOutput,
+    Environment,
+    Tags,
+)
+
+from constructs import Construct
+
+
 from pydantic import BaseSettings
 import os
+
 
 code_dir = (Path(__file__).parent.absolute()).parent.absolute()
 docker_dir = os.path.join(code_dir, 'docker')
 setup_dir = os.path.join(code_dir, 'openaqdb')
+
 env_file = os.path.join(code_dir, ".env")
 
-## settings include both
+
 class Settings(BaseSettings):
     DATABASE_READ_USER: str
     DATABASE_READ_PASSWORD: str
@@ -36,10 +42,10 @@ class Settings(BaseSettings):
 settings = Settings()
 
 
-class dbStack(core.Stack):
+class dbStack(Stack):
     def __init__(
         self,
-        scope: core.Construct,
+        scope: Construct,
         id: str,
         elasticIpAllocationId: str = None,
         keyName: str = None,
@@ -84,7 +90,7 @@ class dbStack(core.Stack):
         # update and install everything that is needed for docker
         UserData = _ec2.UserData.for_linux()
         UserData.add_commands("yum update -y")
-        UserData.add_commands("amazon-linux-extras install docker -y")
+        UserData.add_commands("yum install docker -y")
         UserData.add_commands("service docker start")
         UserData.add_commands("usermod -a -G docker ec2-user")
         UserData.add_commands("chkconfig docker on")
@@ -95,39 +101,50 @@ class dbStack(core.Stack):
             f"{id}-dbstack-database",
             instance_name=f"{id}-dbstack-database",
             instance_type=_ec2.InstanceType("t2.micro"),
-            machine_image=_ec2.MachineImage().lookup(name="amzn2-ami-hvm-2.0.20200520.1-x86_64-gp2"),
+            machine_image=_ec2.MachineImage.latest_amazon_linux(),
             init=_ec2.CloudFormationInit.from_elements(
-                ## Add some files and then build and run the docker image
-                _ec2.InitFile.from_asset("/app/Dockerfile", os.path.join(docker_dir, 'Dockerfile')),
-                ## env data to use for the docker container
+                # Add some files and then build and run the docker image
+                _ec2.InitFile.from_asset(
+                    "/app/Dockerfile",
+                    os.path.join(docker_dir, 'Dockerfile')
+                ),
+                # env data to use for the docker container
                 _ec2.InitFile.from_asset("/app/env", env_file),
-                ## Because of all the subdirectories its easier just to copy everything and unzip it later
+                # Because of all the subdirectories its easier just
+                # to copy everything and unzip it later
                 _ec2.InitFile.from_asset("/app/db.zip", setup_dir),
-                ## Once we copy the files over we need to build and start the instance
-                ## the initfile method does not copy over the permissions by default so
-                ## we need to make the init file executable
-                _ec2.InitCommand.shell_command('cd /app && unzip db.zip -d setup && docker build -t db-instance . && docker run --name db-openaq --env-file env --publish 5432:5432 -idt db-instance'),
+                # Once we copy the files over we need to
+                # build and start the instance
+                # the initfile method does not copy over
+                # the permissions by default so
+                # we need to make the init file executable
+                _ec2.InitCommand.shell_command(
+                     'cd /app && unzip db.zip -d setup && docker build -t db-instance . && docker run --name db-openaq --env-file env --publish 5432:5432 -idt db-instance'
+                ),
             ),
             vpc=vpc,
             security_group=sg,
             key_name=keyName,
-            vpc_subnets=_ec2.SubnetSelection(subnet_type=_ec2.SubnetType.PUBLIC),
+            vpc_subnets=_ec2.SubnetSelection(
+                subnet_type=_ec2.SubnetType.PUBLIC
+            ),
             user_data=UserData
         )
 
         # if we want to assign a specific ip address
         # this can be handy for staging but probably where you
-        # may be destroying and rebuilding a lot but not worth it for production
+        # may be destroying and rebuilding a lot but
+        # not worth it for production
         # where something will be deployed and left alone
         if elasticIpAllocationId is not None:
-            cfn_eIPAssociation = _ec2.CfnEIPAssociation(
+            _ec2.CfnEIPAssociation(
                 self,
                 f"{id}-dbstack-ipaddress",
                 allocation_id=elasticIpAllocationId,
                 instance_id=ec2.instance_id,
             )
 
-        core.CfnOutput(
+        CfnOutput(
             scope=self,
             id=f"{id}-public-ip",
             value=ec2.instance_public_ip,
@@ -135,29 +152,28 @@ class dbStack(core.Stack):
             export_name=f"{id}-public-ip")
 
 
-
-app = core.App()
+app = aws_cdk.App()
 
 staging = dbStack(
     app,
     "staging-db",
-    keyName = settings.KEY_NAME,
-    elasticIpAllocationId = "eipalloc-0a26546dfabd405a1",
-    env=core.Environment(
+    keyName=settings.KEY_NAME,
+    elasticIpAllocationId="eipalloc-0a26546dfabd405a1",
+)
+
+
+cac = dbStack(
+    app,
+    "cac-database",
+    keyName=settings.KEY_NAME,
+    sshIpRange=settings.IP_ADDRESS,
+    env=Environment(
         account=os.getenv('CDK_DEFAULT_ACCOUNT'),
         region=os.getenv('CDK_DEFAULT_REGION')
     ),
 )
 
-cac = dbStack(
-    app,
-    "cac-database",
-    keyName = settings.KEY_NAME,
-    sshIpRange = settings.IP_ADDRESS,
-    env=core.Environment(
-        account=os.getenv('CDK_DEFAULT_ACCOUNT'),
-        region=os.getenv('CDK_DEFAULT_REGION')
-    ),
-)
+Tags.of(cac).add("Project", "CAC")
+Tags.of(staging).add("Project", "OpenAQ")
 
 app.synth()
